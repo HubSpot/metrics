0817b90284d39eca5f11e77d69cbd144c865a390
Jonathan Haber <jhaber@hubspot.com>
2016-10-14 17:28:03 -0400

    Fix test

--- metrics-core/src/test/java/com/yammer/metrics/stats/tests/ExponentiallyDecayingSampleTest.java
+++ metrics-core/src/test/java/com/yammer/metrics/stats/tests/ExponentiallyDecayingSampleTest.java
@@ -112,12 +112,10 @@ public class ExponentiallyDecayingSampleTest {
         assertAllValuesBetween(sample, 1000, 2000);
 
         // wait for 15 hours and add another value.
-        // this should trigger a rescale. Note that the number of samples will be reduced to 2
-        // because of the very small scaling factor that will make all existing priorities equal to
-        // zero after rescale.
+        // this should trigger a rescale. The rescale will drop all values more than 5 minutes old
         clock.addHours(15);
         sample.update(2000);
-        assertThat("the sample has 2 elements", sample.getSnapshot().size(), is(2));
+        assertThat("the sample has 1 element", sample.getSnapshot().size(), is(1));
         assertAllValuesBetween(sample, 1000, 3000);
 
 
