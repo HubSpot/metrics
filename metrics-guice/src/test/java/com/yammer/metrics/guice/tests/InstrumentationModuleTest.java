a32a8009de25e3f0d308e18e9d1608b5111a1730
Jonathan Haber <jhaber@hubspot.com>
2018-12-14 17:16:40 -0500

    Add test

--- metrics-guice/src/test/java/com/yammer/metrics/guice/tests/InstrumentationModuleTest.java
+++ metrics-guice/src/test/java/com/yammer/metrics/guice/tests/InstrumentationModuleTest.java
@@ -1,17 +1,22 @@
 package com.yammer.metrics.guice.tests;
 
+import static org.hamcrest.Matchers.is;
+import static org.hamcrest.Matchers.notNullValue;
+import static org.hamcrest.Matchers.sameInstance;
+import static org.junit.Assert.assertThat;
+
+import org.junit.Test;
+
 import com.google.inject.Guice;
 import com.google.inject.Injector;
 import com.yammer.metrics.HealthChecks;
 import com.yammer.metrics.Metrics;
+import com.yammer.metrics.annotation.Timed;
 import com.yammer.metrics.core.HealthCheckRegistry;
+import com.yammer.metrics.core.MetricName;
 import com.yammer.metrics.core.MetricsRegistry;
+import com.yammer.metrics.core.Timer;
 import com.yammer.metrics.guice.InstrumentationModule;
-import org.junit.Test;
-
-import static org.hamcrest.Matchers.is;
-import static org.hamcrest.Matchers.sameInstance;
-import static org.junit.Assert.assertThat;
 
 public class InstrumentationModuleTest {
     private final InstrumentationModule module = new InstrumentationModule();
@@ -27,5 +32,28 @@ public class InstrumentationModuleTest {
     public void defaultsToTheDefaultHealthCheckRegistry() throws Exception {
         assertThat(injector.getInstance(HealthCheckRegistry.class),
                    is(sameInstance(HealthChecks.defaultRegistry())));
+    }
+
+    @Test
+    public void moduleInstallIsIdempotent() {
+        Injector injector = Guice.createInjector(new InstrumentationModule(), new InstrumentationModule());
+
+        TestTimed instance = injector.getInstance(TestTimed.class);
+
+        MetricName metricName = new MetricName(TestTimed.class, "method");
+        Timer timer = (Timer) Metrics.defaultRegistry().allMetrics().get(metricName);
+
+        assertThat(timer, is(notNullValue()));
+        assertThat(timer.count(), is(0L));
+
+        instance.method();
+
+        assertThat(timer.count(), is(1L));
+    }
+
+    public static class TestTimed {
+
+        @Timed
+        public void method() {}
     }
 }
